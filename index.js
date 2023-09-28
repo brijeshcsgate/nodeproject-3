cabdfc36235efce719df07ff64ca7e849ce1032c
brijeshcsgate <mauryabrijesh34@gmail.com>
2023-09-28 11:00:55 +0530

    commit changes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-aa
+const express=require('express');
+const app=express();
+app.get('/',function(req,res){
+    res.send('it works')
+})
+
+app.listen(process.env.PORT || 5000);
+module.exports=app;
