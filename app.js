5d84c107d0068d5dc7fa3d2936b8d71e43b44d32
BRIJESH MAURYA <48089776+brijeshcsgate@users.noreply.github.com>
2025-01-10 13:28:25 +0530

    krishna

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = 3000;
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
-    res.send('Hello, World with brijesh maurya jai shree ram!');
+    res.send('Hello, World with brijesh maurya jai shree ram jai shri krishna!');
 });
 
 // Start the server and make it listen on the specified port
